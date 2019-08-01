c38c8b58e2264bcde9abf7ce3ccfec3e64aadb22
Damyan Peykov <dppeykov@gmail.com>
2019-08-01 20:58:33 +0200

    Adding some bootstrap components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import Jumbotron from "react-bootstrap/Jumbotron";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <ButtonToolbar>
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="success">Success</Button>
+        <Button variant="warning">Warning</Button>
+        <Button variant="danger">Danger</Button>
+        <Button variant="info">Info</Button>
+        <Button variant="light">Light</Button>
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
+      </ButtonToolbar>
+
+      <Jumbotron>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
     </div>
   );
 }
