32c50143a2624204f4e0b8b46030086ad7d19759
Damyan Peykov <dppeykov@gmail.com>
2019-09-16 15:53:23 +0200

    Alert link

--- src/index.js
+++ src/index.js
@@ -33,38 +33,35 @@ const alerts = [
 function App() {
   return (
     <div className="App">
-      
-        <Navbar collapseOnSelect expand="lg" bg="warning" variant="light">
-          <Navbar.Brand href="#home">Testig</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <Navbar collapseOnSelect expand="lg" bg="warning" variant="light">
+        <Navbar.Brand href="#home">Testig</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-        <div className="container">
+      <div className="container">
         <ButtonToolbar className="mt-3 container">
           <Button variant="primary">Primary Buuton</Button>
           <Button variant="secondary">Secondary Button</Button>
@@ -90,8 +87,10 @@ function App() {
           {alerts.map((variant, idx) => (
             <Alert key={idx} variant={variant}>
               This is a {variant} alert with{" "}
-              <Alert.Link href="#">an example link</Alert.Link>. Give it a click
-              if you like.
+              <Alert.Link href="#">
+                an example link with alert link component
+              </Alert.Link>
+              . Give it a click if you like.
             </Alert>
           ))}
         </>
